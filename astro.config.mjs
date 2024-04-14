81a596f74e024912a8b316deb5e13a4991b89155
noshimorimoshi <noshimorimoshi@mail.ru>
2024-04-14 09:27:57 +0300

    Deploy your Astro Site to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://noshimorimoshi.github.io',
+    base: 'web-speech-api',
+});
